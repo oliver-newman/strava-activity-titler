af4974b77b18db0b54451acb6707dd9086ecf8c7
Oliver Newman <oliver@whmsi.com>
2017-07-06 08:06:45 -0400

    full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # Returns the full title of a page, defaulting to the site name.
+  def full_title(page_title = '')
+    base_title = "Strava Activity Titler"
+    if base_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
