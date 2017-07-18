dc7c5c676e76c1d8b4b95c0fef88e9c7f2c3dc0b
Oliver Newman <oliver@whmsi.com>
2017-07-17 22:06:50 -0400

    Layout and styles

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
 
-  get 'static_pages/home' # TODO: remove this
-  get 'static_pages/faq'
-  get 'static_pages/about'
+  get '/faq', to: 'static_pages#faq'
+  get '/about', to: 'static_pages#about'
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,25 +6,19 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get root" do
-    get root_url
-    assert_response :success
-    assert_select "title", @base_title
-  end
-
-  test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
     assert_select "title", @base_title
   end
 
   test "should get faq" do
-    get static_pages_faq_url
+    get faq_path
     assert_response :success
     assert_select "title", "FAQ | #{@base_title}"
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
     assert_select "title", "About | #{@base_title}"
   end
